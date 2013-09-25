adc30bdc0afad33f744c9a2974b6fd4b2cee62f1
Marek Kopicki <marek.kopicki@gmail.com>
2013-09-26 01:40:10 +0200

    example training data

--- src/pacman/Bham/GraspTest/GraspTest.cpp
+++ src/pacman/Bham/GraspTest/GraspTest.cpp
@@ -3,7 +3,6 @@
 
 using namespace pacman;
 
-
 int main(int argc, char *argv[]) {
 	try {
 		if (argc < 2) {
@@ -11,10 +10,18 @@ int main(int argc, char *argv[]) {
 			return 1;
 		}
 
+		// create grasp
 		BhamGrasp* grasp = BhamGrasp::create(argv[1]);
+		
+		// load and add initial training data
+		Point3D::Seq points;
+		load("pacman_mug1.pcd", points);
+		RobotUIBK::Config::Seq trajectory;
+		load("pacman_mug1.trj", trajectory);
+		grasp->add("pacman_mug1", points, trajectory);
+		
+		// run service here (and comment out the line below)
 		grasp->spin();
-
-		// TODO
 	}
 	catch (const std::exception& ex) {
 		printf("GraspTest exception: %s\n", ex.what());
