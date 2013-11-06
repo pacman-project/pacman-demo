af0bc46e1ed1391fd7ee15a69dc0f1bfe9533612
Marek Kopicki <marek.kopicki@gmail.com>
2013-11-06 14:06:46 +0000

    pacman pose estimation test

--- src/pacman/UIBK/PoseEstimationTest/PoseEstimationTest.cpp
+++ src/pacman/UIBK/PoseEstimationTest/PoseEstimationTest.cpp
@@ -21,6 +21,19 @@ int main(int argc, char *argv[])
                 UIBKPoseEstimation::Pose::Seq poses_;
 
                 pose->estimate(points_,poses_);
+
+				printf("====== OBJECTS ======\n");
+				std::vector<std::string> objects_;
+				object->list(objects_);
+				for (auto i: objects_)
+					printf("%s\n", i.c_str());
+
+				printf("====== POSES ======\n");
+				Point3D::Seq outpoints_;
+				for (auto i: poses_) {
+					object->get(i.id, outpoints_);
+					printf("%s: weight=%f, pose=(%f, %f, %f), size=%d\n", i.id.c_str(), i.weight, i.pose.p.x, i.pose.p.y, i.pose.p.z, outpoints_.size());
+				}
 	}
 	catch (const std::exception& ex) 
         {
