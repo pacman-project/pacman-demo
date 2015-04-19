3a62be60687852902369bb7c076bf0ed1aaefeaf
Marek Kopicki <marek.kopicki@gmail.com>
2015-04-19 01:01:09 +0100

    Model pose estimation

--- DR52/include/pacman/Bham/Demo/Demo.h
+++ DR52/include/pacman/Bham/Demo/Demo.h
@@ -9,6 +9,7 @@
 #define _PACMAN_BHAM_DEMO_DEMO_H_
 
 #include <Grasp/App/Player/Player.h>
+#include <Grasp/Grasp/Model.h>
 
 /** PaCMan name space */
 namespace pacman {
@@ -18,11 +19,68 @@ namespace pacman {
 /** Demo. */
 class Demo : public grasp::Player {
 public:
-	/** Player description */
+	/** Data */
+	class Data : public grasp::Player::Data {
+	public:
+		/** Drainer pose */
+		golem::Mat34 drainerPose;
+		/** Drainer model triangles */
+		grasp::Vec3Seq drainerVertices;
+		/** Drainer model triangles */
+		grasp::TriangleSeq drainerTriangles;
+		/** Drainer model triangles */
+		grasp::Contact3D::Triangle::Seq drainerModelTriangles;
+
+		/** Data bundle description */
+		class Desc : public grasp::Player::Data::Desc {
+		public:
+			/** Creates the object from the description. */
+			virtual grasp::data::Data::Ptr create(golem::Context &context) const;
+		};
+
+		/** Manager */
+		virtual void setOwner(grasp::Manager* owner);
+
+		/** Creates render buffer of the bundle without items */
+		virtual void createRender();
+
+	protected:
+		/** Demo */
+		Demo* owner;
+
+		/** Load from xml context */
+		virtual void load(const std::string& prefix, const golem::XMLContext* xmlcontext, const grasp::data::Handler::Map& handlerMap);
+		/** Save to xml context */
+		virtual void save(const std::string& prefix, golem::XMLContext* xmlcontext) const;
+
+		/** Creates data bundle */
+		void create(const Desc& desc);
+		/** Creates data bundle */
+		Data(golem::Context &context);
+	};
+
+	friend class Data;
+
+	/** Demo description */
 	class Desc : public grasp::Player::Desc {
 	public:
 		typedef golem::shared_ptr<Desc> Ptr;
 
+		/** Data bundle default name */
+		std::string dataName;
+
+		/** Drainer pose estimation camera */
+		std::string drainerCamera;
+		/** Drainer data handler */
+		std::string drainerHandler;
+		/** Drainer data item */
+		std::string drainerItem;
+		/** Drainer scan pose */
+		grasp::ConfigMat34 drainerScanPose;
+		/** Drainer colour solid */
+		golem::RGBA drainerColourSolid;
+		/** Drainer colour wireframe */
+		golem::RGBA drainerColourWire;
 
 		/** Constructs from description object */
 		Desc() {
@@ -32,11 +90,29 @@ public:
 		virtual void setToDefault() {
 			grasp::Player::Desc::setToDefault();
 
+			dataDesc.reset(new Data::Desc);
+
+			dataName.clear();
+
+			drainerCamera.clear();
+			drainerHandler.clear();
+			drainerItem.clear();
+			drainerScanPose.setToDefault();
+			drainerColourSolid.set(golem::RGBA::GREEN._U8[0], golem::RGBA::GREEN._U8[1], golem::RGBA::GREEN._U8[2], golem::numeric_const<golem::U8>::MAX / 8);
+			drainerColourWire.set(golem::RGBA::GREEN);
 		}
 		/** Assert that the description is valid. */
 		virtual void assertValid(const grasp::Assert::Context& ac) const {
 			grasp::Player::Desc::assertValid(ac);
 
+			grasp::Assert::valid(dataDesc != nullptr && grasp::is<Data::Desc>(dataDesc.get()), ac, "dataDesc: unknown type");
+
+			grasp::Assert::valid(dataName.length() > 0, ac, "dataName: invalid");
+
+			grasp::Assert::valid(drainerCamera.length() > 0, ac, "drainerCamera: invalid");
+			grasp::Assert::valid(drainerHandler.length() > 0, ac, "drainerHandler: invalid");
+			grasp::Assert::valid(drainerItem.length() > 0, ac, "drainerItem: invalid");
+			drainerScanPose.assertValid(grasp::Assert::Context(ac, "drainerScanPose."));
 		}
 		/** Load descritpion from xml context. */
 		virtual void load(golem::Context& context, const golem::XMLContext* xmlcontext);
@@ -46,6 +122,28 @@ public:
 	};
 
 protected:
+	/** Data bundle default name */
+	std::string dataName;
+
+	/** Drainer pose estimation camera */
+	grasp::Camera* drainerCamera;
+	/** Drainer data handler */
+	grasp::data::Handler* drainerHandler;
+	/** Drainer data item */
+	std::string drainerItem;
+	/** Drainer scan pose */
+	grasp::ConfigMat34 drainerScanPose;
+
+	/** Drainer colour solid */
+	golem::RGBA drainerColourSolid;
+	/** Drainer colour wireframe */
+	golem::RGBA drainerColourWire;
+	/** Drainer renderer */
+	golem::DebugRenderer drainerRenderer;
+
+	/** golem::UIRenderer interface */
+	virtual void render() const;
+
 	void create(const Desc& desc);
 	Demo(golem::Scene &scene);
 	~Demo();
--- DR52/src/pacman/Bham/Demo/Demo.cpp
+++ DR52/src/pacman/Bham/Demo/Demo.cpp
@@ -1,4 +1,6 @@
 #include <pacman/Bham/Demo/Demo.h>
+#include <Grasp/Core/Import.h>
+#include <Golem/Phys/Data.h>
 
 using namespace pacman;
 using namespace golem;
@@ -6,14 +8,83 @@ using namespace grasp;
 
 //-----------------------------------------------------------------------------
 
-void pacman::Demo::Desc::load(golem::Context& context, const golem::XMLContext* xmlcontext) {
-	Player::Desc::load(context, xmlcontext);
+data::Data::Ptr Demo::Data::Desc::create(golem::Context &context) const {
+	grasp::data::Data::Ptr data(new Demo::Data(context));
+	static_cast<Demo::Data*>(data.get())->create(*this);
+	return data;
+}
 
+Demo::Data::Data(golem::Context &context) : grasp::Player::Data(context), owner(nullptr) {
+}
+
+void Demo::Data::create(const Desc& desc) {
+	Player::Data::create(desc);
+
+	drainerPose.setId();
+	drainerVertices.clear();
+	drainerTriangles.clear();
+	drainerModelTriangles.clear();
+}
+
+void Demo::Data::setOwner(Manager* owner) {
+	grasp::Player::Data::setOwner(owner);
+	this->owner = is<Demo>(owner);
+	if (!this->owner)
+		throw Message(Message::LEVEL_CRIT, "pacman::Demo::Data::setOwner(): unknown data owner");
+}
+
+void Demo::Data::createRender() {
+	Player::Data::createRender();
+	{
+		golem::CriticalSectionWrapper csw(owner->csRenderer);
+		owner->drainerRenderer.reset();
+		owner->drainerRenderer.setColour(owner->drainerColourSolid);
+		owner->drainerRenderer.addSolid(drainerVertices.data(), (U32)drainerVertices.size(), drainerTriangles.data(), (U32)drainerTriangles.size());
+		owner->drainerRenderer.setColour(owner->drainerColourWire);
+		owner->drainerRenderer.addWire(drainerVertices.data(), (U32)drainerVertices.size(), drainerTriangles.data(), (U32)drainerTriangles.size());
+	}
+}
+
+void Demo::Data::load(const std::string& prefix, const golem::XMLContext* xmlcontext, const data::Handler::Map& handlerMap) {
+	data::Data::load(prefix, xmlcontext, handlerMap);
+
+	FileReadStream frs((prefix + this->owner->dataName).c_str());
+	frs.read(drainerPose);
+	frs.read(drainerVertices, drainerVertices.end());
+	frs.read(drainerTriangles, drainerTriangles.end());
+	frs.read(drainerModelTriangles, drainerModelTriangles.end());
+}
+
+void Demo::Data::save(const std::string& prefix, golem::XMLContext* xmlcontext) const {
+	data::Data::save(prefix, xmlcontext);
+
+	FileWriteStream fws((prefix + this->owner->dataName).c_str());
+	fws.write(drainerPose);
+	fws.write(drainerVertices.begin(), drainerVertices.end());
+	fws.write(drainerTriangles.begin(), drainerTriangles.end());
+	fws.write(drainerModelTriangles.begin(), drainerModelTriangles.end());
 }
 
 //------------------------------------------------------------------------------
 
-pacman::Demo::Demo(Scene &scene) : Player(scene) {
+void Demo::Desc::load(golem::Context& context, const golem::XMLContext* xmlcontext) {
+	Player::Desc::load(context, xmlcontext);
+
+	xmlcontext = xmlcontext->getContextFirst("demo");
+
+	golem::XMLData("data_name", dataName, const_cast<golem::XMLContext*>(xmlcontext));
+
+	golem::XMLData("camera", drainerCamera, xmlcontext->getContextFirst("drainer"));
+	golem::XMLData("handler", drainerHandler, xmlcontext->getContextFirst("drainer"));
+	golem::XMLData("item", drainerItem, xmlcontext->getContextFirst("drainer"));
+	drainerScanPose.xmlData(xmlcontext->getContextFirst("drainer scan_pose"));
+	golem::XMLData(drainerColourSolid, xmlcontext->getContextFirst("drainer colour solid"));
+	golem::XMLData(drainerColourWire, xmlcontext->getContextFirst("drainer colour wire"));
+}
+
+//------------------------------------------------------------------------------
+
+pacman::Demo::Demo(Scene &scene) : Player(scene), drainerCamera(nullptr), drainerHandler(nullptr) {
 }
 
 pacman::Demo::~Demo() {
@@ -25,17 +96,108 @@ void pacman::Demo::create(const Desc& desc) {
 	// create object
 	Player::create(desc); // throws
 
+	dataName = desc.dataName;
+
+	grasp::Sensor::Map::const_iterator camera = sensorMap.find(desc.drainerCamera);
+	drainerCamera = camera != sensorMap.end() ? is<Camera>(camera->second.get()) : nullptr;
+	if (!drainerCamera)
+		throw Message(Message::LEVEL_CRIT, "pacman::Demo::create(): unknown drainer pose estimation camera: %s", desc.drainerCamera.c_str());
+	
+	grasp::data::Handler::Map::const_iterator handler = handlerMap.find(desc.drainerHandler);
+	drainerHandler = handler != handlerMap.end() ? handler->second.get() : nullptr;
+	if (!drainerHandler)
+		throw Message(Message::LEVEL_CRIT, "pacman::Demo::create(): unknown drainer data handler: %s", desc.drainerHandler.c_str());
+	
+	drainerItem = desc.drainerItem;
+	drainerScanPose = desc.drainerScanPose;
+	drainerColourSolid = desc.drainerColourSolid;
+	drainerColourWire = desc.drainerColourWire;
 
 	// top menu help using global key '?'
 	scene.getHelp().insert(Scene::StrMapVal("0F5", "  P                                       menu PaCMan\n"));
 
 	// data menu control and commands
 	menuCtrlMap.insert(std::make_pair("P", [=](MenuCmdMap& menuCmdMap, std::string& desc) {
-		desc = "Press a key to: run (D)emo...";
+		desc = "Press a key to: run (D)emo, load and estimate (M)odel...";
 	}));
+	
+	// drainer pose scan and estimation
+	menuCmdMap.insert(std::make_pair("PM", [=]() {
+		UI::removeCallback(*this, getCurrentHandler());
+		{
+			RenderBlock renderBlock(*this);
+			golem::CriticalSectionWrapper cswData(csData);
+			to<Data>(dataCurrentPtr)->drainerModelTriangles.clear();
+		}
+		// run robot
+		gotoPose(drainerScanPose);
+		// obtain capture interface
+		data::Handler::Map::const_iterator handlerSnapshotPtr = handlerMap.find(drainerCamera->getSnapshotHandler());
+		if (handlerSnapshotPtr == handlerMap.end())
+			throw Message(Message::LEVEL_ERROR, "Unknown snapshot handler %s", drainerCamera->getSnapshotHandler().c_str());
+		data::Capture* capture = is<data::Capture>(handlerSnapshotPtr);
+		if (!capture)
+			throw Message(Message::LEVEL_ERROR, "Handler %s does not support Capture interface", drainerCamera->getSnapshotHandler().c_str());
+		// capture and insert data
+		data::Item::Map::iterator ptr;
+		{
+			RenderBlock renderBlock(*this);
+			golem::CriticalSectionWrapper cswData(csData);
+			to<Data>(dataCurrentPtr)->itemMap.erase(drainerItem);
+			ptr = to<Data>(dataCurrentPtr)->itemMap.insert(to<Data>(dataCurrentPtr)->itemMap.end(), data::Item::Map::value_type(drainerItem, capture->capture(*drainerCamera, [&](const grasp::TimeStamp*) -> bool { return true; })));
+			Data::View::setItem(to<Data>(dataCurrentPtr)->itemMap, ptr, to<Data>(dataCurrentPtr)->getView());
+		}
+		// generate features
+		data::Transform* transform = is<data::Transform>(handlerSnapshotPtr);
+		if (!transform)
+			throw Message(Message::LEVEL_ERROR, "Handler %s does not support Transform interface", drainerCamera->getSnapshotHandler().c_str());
+		data::Item::List list;
+		list.insert(list.end(), ptr);
+		data::Item::Ptr item = transform->transform(list);
+		{
+			RenderBlock renderBlock(*this);
+			golem::CriticalSectionWrapper cswData(csData);
+			to<Data>(dataCurrentPtr)->itemMap.erase(drainerItem);
+			ptr = to<Data>(dataCurrentPtr)->itemMap.insert(to<Data>(dataCurrentPtr)->itemMap.end(), data::Item::Map::value_type(drainerItem, item));
+			Data::View::setItem(to<Data>(dataCurrentPtr)->itemMap, ptr, to<Data>(dataCurrentPtr)->getView());
+		}
+		// estimate pose
+		data::Model* model = is<data::Model>(ptr);
+		if (!model)
+			throw Message(Message::LEVEL_ERROR, "Handler %s does not support Model interface", drainerCamera->getSnapshotHandler().c_str());
+		grasp::ConfigMat34 robotPose;
+		golem::Mat34 drainerPose;
+		Vec3Seq drainerVertices;
+		TriangleSeq drainerTriangles;
+		model->model(robotPose, drainerPose, &drainerVertices, &drainerTriangles);
+		// create triangle mesh
+		{
+			RenderBlock renderBlock(*this);
+			golem::CriticalSectionWrapper cswData(csData);
+			to<Data>(dataCurrentPtr)->drainerVertices = drainerVertices;
+			to<Data>(dataCurrentPtr)->drainerTriangles = drainerTriangles;
+			for (grasp::TriangleSeq::const_iterator j = drainerTriangles.begin(); j != drainerTriangles.end(); ++j)
+				to<Data>(dataCurrentPtr)->drainerModelTriangles.push_back(Contact3D::Triangle(drainerVertices[j->t1], drainerVertices[j->t3], drainerVertices[j->t2]));
+		}
+		// done
+	}));
+	
+	// main demo
 	menuCmdMap.insert(std::make_pair("PD", [=]() {
-		// TODO
+		// estimate pose
+		if (!to<Data>(dataCurrentPtr)->drainerModelTriangles.empty())
+			menuCmdMap["PM"]();
+
+		// run demo
 	}));
+}
+
+//------------------------------------------------------------------------------
+
+void pacman::Demo::render() const {
+	Player::render();
+	golem::CriticalSectionWrapper cswRenderer(csRenderer);
+	drainerRenderer.render();
 }
 
 //------------------------------------------------------------------------------
