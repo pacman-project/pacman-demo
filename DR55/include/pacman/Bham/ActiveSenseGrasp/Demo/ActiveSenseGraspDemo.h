d0f9dd290b9ba4c9b68a73fda1d0457baa324c96
eaa3 <ermano.arruda@gmail.com>
2016-03-21 23:59:36 +0000

    removing redundant methods

--- DR55/include/pacman/Bham/ActiveSenseGrasp/Demo/ActiveSenseGraspDemo.h
+++ DR55/include/pacman/Bham/ActiveSenseGrasp/Demo/ActiveSenseGraspDemo.h
@@ -124,26 +124,13 @@ public:
 
     void showRecordingState();
 
-    //grasp::Camera* getWristCamera(const bool dontThrow = false) const;
-    //golem::Mat34 getWristPose() const;
-    //golem::Controller::State::Seq getTrajectoryFromPose(const golem::Mat34& w, const golem::SecTmReal duration);
-    //grasp::ConfigMat34 getConfigFromPose(const golem::Mat34& w);
-    //golem::Controller::State lookupStateArmCommandHand() const;
-
     void setHandConfig(golem::Controller::State::Seq& trajectory, const golem::Controller::State cmdHand);
-    //void setHandConfig(golem::Controller::State::Seq& trajectory, const grasp::ConfigMat34& handPose);
-    //void gotoWristPose(const golem::Mat34& w, const golem::SecTmReal duration = golem::SEC_TM_REAL_ZERO);
-    void gotoPose2(const grasp::ConfigMat34& pose, const golem::SecTmReal duration, const bool ignoreHand = false);
-    //void releaseHand(const double openFraction, const golem::SecTmReal duration);
-    void releaseHand2(const double openFraction, const golem::SecTmReal duration, const golem::Controller::State partReleaseConfig);
-    //void closeHand(const double closeFraction, const golem::SecTmReal duration);
 
-    //void nudgeWrist();
-    //void rotateObjectInHand();
+    void gotoPose2(const grasp::ConfigMat34& pose, const golem::SecTmReal duration, const bool ignoreHand = false);
+
+    void releaseHand2(const double openFraction, const golem::SecTmReal duration, const golem::Controller::State partReleaseConfig);
 
     void executeDropOff();
-
-
     void setMenus();
 
     virtual bool gotoPoseWS(const grasp::ConfigMat34& pose, const golem::Real& linthr = 0.0000001, const golem::Real& angthr = 0.0000001);
